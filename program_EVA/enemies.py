48b93b3329704c7b1228982550043944c8fcf163
MiniTacos <borodinka234@mail.ru>
2024-11-24 23:27:17 +0300

    last changes

--- program_EVA/enemies.py
+++ program_EVA/enemies.py
@@ -11,7 +11,7 @@ class Enemy():
 
     def appearance(self):
         """Отображение врага, через которого надо прыгать"""
-        pygame.draw.rect(display, (45, 237, 123), (self.horizontal_coordinate, self.vertical_coordinate, self.width, self.height))
+        pygame.draw.rect(display, (52, 228, 123), (self.horizontal_coordinate, self.vertical_coordinate, self.width, self.height))
 
     def movement(self, move):
         """Передвижение врага"""
